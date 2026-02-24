dd9ffb9b9e272f66838624d2cee5816593c88024
Rishi Verma <rishiv1000@outlook.com>
2026-02-24 23:33:37 +0530

    functional interface

---
+++ Java8/code5_functional_interface1.java
@@ -0,0 +1,12 @@
+
+package Java8;
+
+//A functional interface should consist of exactly one incomplete method inside it
+
+@FunctionalInterface
+public interface code5_functional_interface1 {
+    public void pri();
+
+    // public void s(); --> cause error because of it is f. i.
+    
+}
---
+++ Java8/code6_functional_interface2.java
@@ -0,0 +1,24 @@
+package Java8;
+
+@FunctionalInterface
+public interface code6_functional_interface2 {
+    default void fun1(){
+        System.out.println("this is function interface default method");
+    }
+     void fun2();
+    
+}
+
+class A implements code6_functional_interface2{
+     @Override
+    public void fun2(){
+             System.out.print("this is the overridden method of the f. i.");
+      }
+    public static void main(String[] args) {
+       A ob= new A();
+       ob.fun1();
+       ob.fun2();
+    }
+
+    
+}
